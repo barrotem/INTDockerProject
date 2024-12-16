4069c0846969c74abf13ddb4d0620f3f19ce2ea8
BarRotem <bar.rotem45@gmail.com>
2024-12-16 20:34:33 +0200

    implement yolo5 - prediction #1

--- yolo5/app.py
+++ yolo5/app.py
@@ -25,16 +25,17 @@ def predict():
     # Receives a URL parameter representing the image to download from S3
     img_name = request.args.get('imgName')
     img_path = Path(img_name)
-    # TODO download img_name from S3, store the local image path in the original_img_path variable.
-    #  The bucket name is provided as an env var BUCKET_NAME.
+    # Initialize s3 resource
     s3_resource = boto3.client("s3")
 
+    # Download and store the remote image under predictions/<image_filename>
     original_img_path = Path(f'predictions/{img_path.name}')
     if not os.path.exists(original_img_path.parent):
+        # Create predictions directory if it doesn't already exist
         os.makedirs(original_img_path.parent)
-    logger.info(f'downloading based on the following strings:,{images_bucket},{img_name},{str(original_img_path)}')
+    logger.info(f'downloading image based on the following strings (bucket, bucket_image_path, destionation_local_path):,'
+                f'{images_bucket},{img_name},{str(original_img_path)}')
     s3_resource.download_file(Bucket=images_bucket, Key=img_name, Filename=str(original_img_path))
-
     logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
     # Predicts the objects in the image
@@ -46,18 +47,19 @@ def predict():
         name=prediction_id,
         save_txt=True
     )
-
     logger.info(f'prediction: {prediction_id}/{original_img_path}. done')
 
     # This is the path for the predicted image with labels
     # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
     predicted_img_path = Path(f'static/data/{prediction_id}/{original_img_path.name}')
-
-    # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
+    # Uploads the predicted image (predicted_img_path) to S3 - without accidentally overriding the original image.
     s3_resource.upload_file(Filename=str(predicted_img_path), Bucket=images_bucket, Key=str(original_img_path))
+
     # Parse prediction labels and create a summary
-    pred_summary_path = Path(f'static/data/{prediction_id}/labels/{str(original_img_path).split(".")[0]}.txt')
-    #TODO : Understand why the above path doesn't exist, that's what the code says
+    pred_summary_path = Path(f'static/data/{prediction_id}/labels/{str(predicted_img_path.name).split(".")[0]}.txt')
+    logger.info(f'looking for prediction summary in path: {pred_summary_path}...')
+    # The file located in pred_summary_path represents the model's prediction results.
+    # Parse prediction results into a JSON like object and return the result.
     if pred_summary_path.exists():
         with open(pred_summary_path) as f:
             labels = f.read().splitlines()
@@ -74,14 +76,14 @@ def predict():
 
         prediction_summary = {
             'prediction_id': prediction_id,
-            'original_img_path': original_img_path,
-            'predicted_img_path': predicted_img_path,
+            'original_img_path': str(original_img_path),
+            'predicted_img_path': str(predicted_img_path),
             'labels': labels,
             'time': time.time()
         }
 
         # TODO store the prediction_summary in MongoDB
-
+        # Return prediction_summary as a response to the request
         return prediction_summary
     else:
         return f'prediction: {prediction_id}/{original_img_path}. prediction result not found', 404
