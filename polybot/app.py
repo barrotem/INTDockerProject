44d66c83ceb5596bbbf5bde8a0783207d58abe3d
BarRotem <bar.rotem45@gmail.com>
2024-12-30 22:16:58 +0200

    docker-compose : using secrets

--- polybot/app.py
+++ polybot/app.py
@@ -5,9 +5,15 @@ from bot import ObjectDetectionBot
 
 app = flask.Flask(__name__)
 
-# Define Telegram Bot required variables
-TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+# Acquire telegram secret files path from environment varibles
+TELEGRAM_TOKEN_PATH = os.environ['TELEGRAM_TOKEN_PATH']
+TELEGRAM_APP_URL_PATH = os.environ['TELEGRAM_APP_URL_PATH']
+# Read and turn secret file contents to the appropriate static variables
+with open(TELEGRAM_TOKEN_PATH) as telegram_token_file:
+    TELEGRAM_TOKEN = telegram_token_file.read()
+with open(TELEGRAM_APP_URL_PATH) as telegram_app_url_file:
+    TELEGRAM_APP_URL = telegram_app_url_file.read()
+
 # Define AWS interaction required varibales
 IMAGES_BUCKET = os.environ['BUCKET_NAME']
 
