4cbb620b9fd97322f2e835c9a4800f2a301b5cf3
BarRotem <bar.rotem45@gmail.com>
2025-01-03 09:03:52 +0200

    CI/CD - dummy commit : fix mongo_init.sh

--- mongo/mongo_init.sh
+++ mongo/mongo_init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Simple bash script to initiate a replica-set "mongo_rs" of 3 mongo-db containers named : "mongo1-3"
 # This script will be used by docker-compose.
-
+echo "Sleeping in order to ensure mongo containers are up ..."
+sleep 5
 echo "Attempting to initiate replica-set ..."
 mongo_state=$(mongosh --host mongo1 --eval "rs.initiate({
  _id: \"mongo_rs\",
